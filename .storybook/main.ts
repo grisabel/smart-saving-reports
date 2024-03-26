768c71ef8bf9dfb30f5887538beb9c019b2c4a56
IsabelGR <grisabel97@outlook.com>
2024-03-26 20:33:12 +0100

    chore/link: config storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { mergeConfig } from "vite";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -14,6 +16,18 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  async viteFinal(config, { configType }) {
+    // Retorna tu configuración personalizada de Vite aquí
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          // Configura tus alias aquí
+          "@": path.resolve(__dirname, "../src"),
+        },
+      },
+    });
   },
 };
 export default config;
