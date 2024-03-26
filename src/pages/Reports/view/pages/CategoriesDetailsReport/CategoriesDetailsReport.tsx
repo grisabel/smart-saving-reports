43ccb3026e181d75bbc9cdd98af5b3156aca0f84
Delgado Trueba <jose.delgado@babel.es>
2024-03-26 22:35:45 +0100

    chore/mfe: add webcomponent

--- src/pages/Reports/view/pages/CategoriesDetailsReport/CategoriesDetailsReport.tsx
+++ src/pages/Reports/view/pages/CategoriesDetailsReport/CategoriesDetailsReport.tsx
@@ -1,5 +1,29 @@
+import WithApp from "@/WithApp";
+import ReactDOM from "react-dom/client";
+
 const CategoriesDetailsReport: React.FC = () => {
-    return ( <h1>CategoriesDetailsReport</h1> );
-}
+  return <h1>CategoriesDetailsReport</h1>;
+};
 
 export default CategoriesDetailsReport;
+
+class CategoriesDetailsReportMfe extends HTMLElement {
+  app: any;
+
+  connectedCallback() {
+    const AppMfe = WithApp(CategoriesDetailsReport);
+    this.app = ReactDOM.createRoot(this);
+    this.app.render(<AppMfe />);
+  }
+
+  disconnectedCallback() {
+    if (this.app) {
+      this.app.unmount();
+    }
+  }
+}
+
+customElements.define(
+  "categories-details-report-mfe",
+  CategoriesDetailsReportMfe
+);
--- src/pages/Reports/view/pages/CategoriesReport/CategoriesReport.tsx
+++ src/pages/Reports/view/pages/CategoriesReport/CategoriesReport.tsx
@@ -1,5 +1,27 @@
+import WithApp from "@/WithApp";
+import ReactDOM from "react-dom/client";
+
 const CategoriesReport: React.FC = () => {
-    return ( <h1>CategoriesReport</h1> );
-}
+  console.log("CategoriesReport");
+  return <h1>CategoriesReport</h1>;
+};
 
 export default CategoriesReport;
+
+class CategoriesReportMfe extends HTMLElement {
+  app: any;
+
+  connectedCallback() {
+    const AppMfe = WithApp(CategoriesReport);
+    this.app = ReactDOM.createRoot(this);
+    this.app.render(<AppMfe />);
+  }
+
+  disconnectedCallback() {
+    if (this.app) {
+      this.app.unmount();
+    }
+  }
+}
+
+customElements.define("categories-report-mfe", CategoriesReportMfe);
