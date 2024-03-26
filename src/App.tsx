aebe1fda3929e65647bb923f37a66ed78b332ae4
Delgado Trueba <jose.delgado@babel.es>
2024-03-26 18:51:37 +0100

    chore/develop: add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "@/styles/globals.scss";
+
 
 function App() {
   const [count, setCount] = useState(0)
