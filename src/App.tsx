c754ed0097196da5c40549e0d66dc304fdce15bc
Delgado Trueba <jose.delgado@babel.es>
2024-03-26 21:22:26 +0100

    chore/develop: create WithApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { useEffect } from "react";
 import "./App.css";
-import "@/styles/globals.scss";
 import { useTranslation } from "react-i18next";
 import AppRouter from "./AppRouter";
 import AppProvider from "./AppProvider";
 
 function App() {
-  const { t, i18n } = useTranslation();
-
-  useEffect(() => {
-    let lang = window.localStorage.getItem("language") || "es";
-    i18n.changeLanguage(lang);
-  }, []);
+  const { t } = useTranslation();
 
   return (
     <AppProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import WithApp from "./WithApp.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const AppCore = WithApp(App);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppCore />
+  </React.StrictMode>
+);
