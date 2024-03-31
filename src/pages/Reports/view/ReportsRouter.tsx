6229564fb1d4cd2a6879474f57fedf3ad7004616
Delgado Trueba <jose.delgado@babel.es>
2024-03-31 20:01:25 +0200

    feat/INF-3.2: fix routing

--- src/pages/Reports/view/ReportsRouter.tsx
+++ src/pages/Reports/view/ReportsRouter.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import React from "react";
+import React, { Suspense } from "react";
 import { ROUTES_REPORTS } from "./routes";
+import Spinner from "@/components/stories/atoms/Spinner/Spinner";
 
 // import CategoriesDetailsReport from "./pages/CategoriesDetailsReport";
 // import CategoriesReport from "./pages/CategoriesReport";
@@ -12,18 +13,20 @@ const CategoriesReport = React.lazy(() => import("./pages/CategoriesReport"));
 
 function ReportsRouter() {
   return (
-    <Routes>
-      <Route
-        path={`${ROUTES_REPORTS.DETAILS}`}
-        element={<CategoriesDetailsReport />}
-      />
-      <Route
-        path={`${ROUTES_REPORTS.SUMMARY}`}
-        element={<CategoriesReport />}
-      />
+    <Suspense fallback={<Spinner open />}>
+      <Routes>
+        <Route
+          path={`${ROUTES_REPORTS.DETAILS}`}
+          element={<CategoriesDetailsReport />}
+        />
+        <Route
+          path={`${ROUTES_REPORTS.SUMMARY}`}
+          element={<CategoriesReport />}
+        />
 
-      <Route path="*" element={<Navigate to={ROUTES_REPORTS.SUMMARY} />} />
-    </Routes>
+        <Route path="*" element={<Navigate to={ROUTES_REPORTS.SUMMARY} />} />
+      </Routes>
+    </Suspense>
   );
 }
 
